7f89ae40838aee09e2d8e2a8146555b1afd5f778
jase <jase@beeslave.releasethebees.xyz>
2023-11-05 19:31:21 +0000

    fixed cookies

--- cookies.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-sites = {}
-#sites['filelist'] = {"uid":"1287353", "pass":"0aa041d24b28b720fd3cb8c38ad59d82", "username":"rotationalvelociraptor", "password":"CDL6t38qmBiZKTyc2Ncj"}
-
-passes = {}
-passes['filelist'] = {"username":"rotationalvelociraptor", "password":"CDL6t38qmBiZKTyc2Ncj"}
-
-
